874dc9bbc77a747e7c8366683a4dedee6fc0efc7
Student <example@example.com>
2019-08-20 16:36:57 -0700

    learn jwt.

---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+class Seed
+  def self.begin
+    Destination.destroy_all
+    seed = Seed.new
+    seed.generate_destinations
+  end
+  def generate_destinations
+    25.times do |i|
+      destination = Destination.create!(
+        city: Faker::Nation.capital_city,
+        country: Faker::Games::Pokemon.location
+      )
+      puts "Destination #{i}: City is #{destination.city} and country is #{destination.country}."
+      rand(30...273).times do |x|
+        review = destination.reviews.create!(
+          name: Faker::FunnyName.name,
+          text_body: Faker::GreekPhilosophers.quote
+        )
+        puts "Review #{x}: User is #{review.name}, text is #{review.text_body}."
+      end
+    end
+  end
+end
+
+Seed.begin
